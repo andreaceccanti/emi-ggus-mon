b8d00b6ed44b10f449d31d9ba3e58d175d77654f
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2019-09-30 07:13:39 +0200

    Fix error handling

--- src/emi_ggus_mon/ws.py
+++ src/emi_ggus_mon/ws.py
@@ -9,12 +9,16 @@ from suds.client import Client
 from suds.plugin import MessagePlugin
 import re
 
-## This is due to a bug in the GGUS web service:
-## https://ggus.eu/ws/ticket_info.php?ticket=77698
+# This is due to a bug in the GGUS web service:
+# https://ggus.eu/ws/ticket_info.php?ticket=77698
+
+
 class FixRemindOnFieldPlugin(MessagePlugin):
-    def received(self,context):
-        (reply, num_changes) = re.subn(r'<ns0:GHD_Remind_On>.*</ns0:GHD_Remind_On>', '', context.reply)
+    def received(self, context):
+        (reply, num_changes) = re.subn(
+            r'<ns0:GHD_Remind_On>.*</ns0:GHD_Remind_On>', '', context.reply)
         context.reply = reply
+
 
 help_desk_url = "https://prod-ars.ggus.eu/arsys/WSDL/public/prod-ars/GGUS"
 history_url = "https://prod-ars.ggus.eu/arsys/WSDL/public/prod-ars/Grid_History"
@@ -22,19 +26,22 @@ history_url = "https://prod-ars.ggus.eu/arsys/WSDL/public/prod-ars/Grid_History"
 username = "emiuser"
 password = "HpL6xVhu?"
 
+
 def init_ggus_client(url):
     p = FixRemindOnFieldPlugin()
 
     c = Client(url, plugins=[p])
     auth_token = c.factory.create("AuthenticationInfo")
-    auth_token.userName=username
-    auth_token.password=password
+    auth_token.userName = username
+    auth_token.password = password
     c.set_options(soapheaders=auth_token)
     return c
+
 
 def get_ticket(ticket_id):
     ggus_client = init_ggus_client(help_desk_url)
     return ggus_client.service.TicketGet(ticket_id)
+
 
 def get_tickets2(query1, query2):
     retval = []
@@ -51,6 +58,7 @@ def get_tickets2(query1, query2):
 
     return retval
 
+
 def get_tickets(query, start=0, limit=-1):
     retval = []
     ggus_client = init_ggus_client(help_desk_url)
@@ -62,9 +70,11 @@ def get_tickets(query, start=0, limit=-1):
 
     return retval
 
+
 def get_ticket_history(ggus_ticket):
     history_client = init_ggus_client(history_url)
     return history_client.service.OpGetTicketHist(ggus_ticket)
+
 
 def cnaf_tickets():
     ggus_client = init_ggus_client(help_desk_url)
@@ -76,12 +86,12 @@ def cnaf_tickets():
         su_result = []
         try:
             su_result = ggus_client.service.TicketGetList(GHD_Responsible_Unit=su,
-                                                        GHD_Meta_Status="Open")
+                                                          GHD_Meta_Status="Open")
         except WebFault, f:
-            ## For some absurd reason the ggus web service raises an error when
-            ## a query has no results, instead of simply returning an
-            ## empty result
-            if not (f.fault is None) and repr(f.fault.faultstring) != u'ERROR (302): Entry does not exist in database; ':
+            # For some absurd reason the ggus web service raises an error when
+            # a query has no results, instead of simply returning an
+            # empty result
+            if not (f.fault is None) and not (u'ERROR (302): Entry does not exist in database; ' in repr(f.fault.faultstring)):
                 raise f
 
         print "%s open tickets : %d" % (su, len(su_result))
@@ -94,8 +104,3 @@ def cnaf_tickets():
         retval.append(ticket)
 
     return retval
-
-
-
-    
-
